b951e0ce533530b864ce04ddf1f57ef63d7c7fd3
Andrew Price <andrew.j.price@gmail.com>
2022-11-27 23:17:01 -0500

    slice updates

--- game.go
+++ game.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 )
 
+var lettersAvailable []string
 var lettersGray []string
 var lettersGreen []string
 var lettersYellow []string
 var lettersInWord [5]string
+var sliceOfWord []string
 
 func gameHandler() {
-	populateGrayLetters()
-	populateLettersInWord()
-	result := false
-	word := getWord()
+	populateAvailableLetters()
+	populateLettersInWord() // defaults to _ _ _ _ _
+	result := false         // defaults to game loss
+	word := getWord()       // gets randomly generated word
+	sliceOfWord = sliceOfString(word)
 	if debugFlow {
 		LoggerDebug.Printf("Game word: %v\n", word)
 	}
@@ -31,6 +35,18 @@ func gameHandler() {
 }
 
 func analyzeWord(guessed string, actual string) bool {
+	guessSlice := sliceOfString(guessed)
+	actualSlice := sliceOfString(actual)
+	fmt.Printf("guessSlice: is of type: %v, with value: %v\n", reflect.TypeOf(guessSlice), guessSlice)
+	fmt.Printf("actualSlice: is of type: %v, with value: %v\n", reflect.TypeOf(actualSlice), actualSlice)
+	/*
+		for acutal_i, acutal_v := range actualSlice {
+			for guess_i, guess_v := range guessSlice {
+
+
+			}
+		}
+	*/
 	if guessed == actual {
 		fmt.Printf("Guessed: %s, Actual: %s\n", guessed, actual)
 		return true
@@ -46,34 +62,34 @@ func getWord() string {
 	return wordList[randomIndex]
 }
 
-func populateGrayLetters() {
+func populateAvailableLetters() {
 	// there probably is a more efficient way
-	lettersGray = append(lettersGray, "a")
-	lettersGray = append(lettersGray, "b")
-	lettersGray = append(lettersGray, "c")
-	lettersGray = append(lettersGray, "d")
-	lettersGray = append(lettersGray, "e")
-	lettersGray = append(lettersGray, "f")
-	lettersGray = append(lettersGray, "g")
-	lettersGray = append(lettersGray, "h")
-	lettersGray = append(lettersGray, "i")
-	lettersGray = append(lettersGray, "j")
-	lettersGray = append(lettersGray, "k")
-	lettersGray = append(lettersGray, "l")
-	lettersGray = append(lettersGray, "m")
-	lettersGray = append(lettersGray, "n")
-	lettersGray = append(lettersGray, "o")
-	lettersGray = append(lettersGray, "p")
-	lettersGray = append(lettersGray, "q")
-	lettersGray = append(lettersGray, "r")
-	lettersGray = append(lettersGray, "s")
-	lettersGray = append(lettersGray, "t")
-	lettersGray = append(lettersGray, "u")
-	lettersGray = append(lettersGray, "v")
-	lettersGray = append(lettersGray, "w")
-	lettersGray = append(lettersGray, "x")
-	lettersGray = append(lettersGray, "y")
-	lettersGray = append(lettersGray, "z")
+	lettersAvailable = append(lettersAvailable, "a")
+	lettersAvailable = append(lettersAvailable, "b")
+	lettersAvailable = append(lettersAvailable, "c")
+	lettersAvailable = append(lettersAvailable, "d")
+	lettersAvailable = append(lettersAvailable, "e")
+	lettersAvailable = append(lettersAvailable, "f")
+	lettersAvailable = append(lettersAvailable, "g")
+	lettersAvailable = append(lettersAvailable, "h")
+	lettersAvailable = append(lettersAvailable, "i")
+	lettersAvailable = append(lettersAvailable, "j")
+	lettersAvailable = append(lettersAvailable, "k")
+	lettersAvailable = append(lettersAvailable, "l")
+	lettersAvailable = append(lettersAvailable, "m")
+	lettersAvailable = append(lettersAvailable, "n")
+	lettersAvailable = append(lettersAvailable, "o")
+	lettersAvailable = append(lettersAvailable, "p")
+	lettersAvailable = append(lettersAvailable, "q")
+	lettersAvailable = append(lettersAvailable, "r")
+	lettersAvailable = append(lettersAvailable, "s")
+	lettersAvailable = append(lettersAvailable, "t")
+	lettersAvailable = append(lettersAvailable, "u")
+	lettersAvailable = append(lettersAvailable, "v")
+	lettersAvailable = append(lettersAvailable, "w")
+	lettersAvailable = append(lettersAvailable, "x")
+	lettersAvailable = append(lettersAvailable, "y")
+	lettersAvailable = append(lettersAvailable, "z")
 }
 
 func populateLettersInWord() {
@@ -87,6 +103,7 @@ func populateLettersInWord() {
 func renderTerminal(attempt int) {
 	fmt.Printf("\n\n")
 	fmt.Printf("On try %d out of 6\n", attempt)
+	fmt.Printf("Available letters: %v\n", lettersAvailable)
 	fmt.Printf("Gray letters: %v\n", lettersGray)
 	fmt.Printf("Green letters: %v\n", lettersGreen)
 	fmt.Printf("Yellow letters: %v\n", lettersYellow)
@@ -103,6 +120,16 @@ func renderTerminalFinal(game bool, word string) {
 		fmt.Println("Better luck next time.")
 	}
 	fmt.Printf("\n\n")
+}
+
+func sliceOfString(word string) []string {
+	var wordSlice []string
+	for _, r := range word {
+		fmt.Println(string(r))
+		wordSlice = append(wordSlice, string(r))
+	}
+	fmt.Printf("wordSlice: is of type: %v, with value: %v\n", reflect.TypeOf(wordSlice), wordSlice)
+	return wordSlice
 }
 
 func userInput() string {
